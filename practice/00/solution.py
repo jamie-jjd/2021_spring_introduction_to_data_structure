3e25843e7bba54bd98372057c5bd34141408bc53
zeroTsai0308 <79447902+zeroTsai0308@users.noreply.github.com>
2021-02-22 17:58:28 +0800

    add practice-0 python solution

--- practice/00/solution.py
+++ practice/00/solution.py
@@ -1,29 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
 
 # Complete the reverseArray function below.
-def reverseArray(a):
-    
+
+# alternative 1
+def reverseArray(a):    
     return(reversed(a))
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    arr_count = int(input())
-
-    arr = list(map(int, input().rstrip().split()))
-
-    res = reverseArray(arr)
-
-    fptr.write(' '.join(map(str, res)))
-    fptr.write('\n')
-
-    fptr.close()
-
+# alternative 2
+def reverseArray(a):
+    a.reverse()
+    return(a)
