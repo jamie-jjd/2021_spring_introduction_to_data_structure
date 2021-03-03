a6438a80f4c059027a7dfbcdef117c817b132ef6
jamie <jinjiedeng.jjd@gmail.com>
2021-03-03 15:27:29 +0800

    add c++ solution

--- practice/01/solution.cc
+++ practice/01/solution.cc
@@ -1,1 +1,17 @@
-// to be announced
+vector<string> Search (vector<long> A, vector<long> Q)
+{
+  vector<string> R;
+  sort(A.begin(), A.end());
+  for (auto const &q : Q)
+  {
+    if (binary_search(A.begin(), A.end(), q))
+    {
+      R.push_back("yes");
+    }
+    else
+    {
+      R.push_back("no");
+    }
+  }
+  return R;
+}
