9e0fb3e434a5faa0a5719c1347e75449ca2ee4c5
jamie <jinjiedeng.jjd@gmail.com>
2021-04-23 08:26:26 +0800

    (1) update DFS & RecursiveDFS (2) fix typos

--- practice/06/solution.cc
+++ practice/06/solution.cc
@@ -1,9 +1,5 @@
 // alternative 1 (BFS) provided by TA 鄧晉杰
-template
-<
-  typename Graph,
-  typename IsDiscovered
->
+template <typename Graph, typename IsDiscovered>
 void BFS
 (
   Graph const &graph,
@@ -16,7 +12,7 @@ void BFS
   nodes.emplace_back(source);
   while (!nodes.empty())
   {
-    auto const from {nodes.front()}; nodes.pop_front(); // the only difference from DFS
+    auto const from {nodes.front()}; nodes.pop_front();
     for (auto const to : graph[from])
     {
       if (!is_discovered[to])
@@ -24,58 +20,6 @@ void BFS
         is_discovered[to] = true;
         nodes.emplace_back(to);
       }
-    }
-  }
-  return;
-}
-
-// alternative 2 (DFS) provided by TA 鄧晉杰
-template <typename Graph, typename IsDiscovered>
-void DFS
-(
-  Graph const &graph,
-  IsDiscovered &is_discovered,
-  long const source
-)
-{
-  std::deque<long> nodes;
-  is_discovered[source] = true;
-  nodes.emplace_back(source);
-  while (!nodes.empty())
-  {
-    auto const from {nodes.back()}; nodes.pop_back(); // the only difference from BFS
-    for (auto const &to : graph[from])
-    {
-      if (!is_discovered[to])
-      {
-        is_discovered[to] = true;
-        nodes.emplace_back(to);
-      }
-    }
-  }
-  return;
-}
-
-// alternative 3 (DFS - recursive version) provided by TA 鄧晉杰
-/* note: this alternative is limited if depth of recursion is too large */
-template
-<
-  typename Graph,
-  typename IsDiscovered
->
-void RecursiveDFS
-(
-  Graph const &graph,
-  IsDiscovered &is_discovered,
-  long const from
-)
-{
-  is_discovered[from] = true;
-  for (auto const &to : graph[from])
-  {
-    if (!is_discovered[to])
-    {
-      RecursiveDFS(graph, is_discovered, to);
     }
   }
   return;
@@ -96,10 +40,7 @@ string CheckIfConnected
     graph[edge[1]].emplace_back(edge[0]);
   }
   std::vector<bool> is_discovered(nodes_size, false);
-  /* please select only 1 among 3 alternatives */
-  BFS(graph, is_discovered, x); /* alternative 1 */
-  // DFS(graph, is_discovered, x); /* alternative 2 */
-  // RecursiveDFS(graph, is_discovered, x); /* alternative 3 */
+  BFS(graph, is_discovered, x);
   if (is_discovered[y])
   {
     return "yes";
@@ -107,7 +48,101 @@ string CheckIfConnected
   return "no";
 }
 
-// alternative (DFS) provided by TA 李旺陽
+// alternative 2 (DFS) provided by TA 鄧晉杰
+template <typename Graph, typename IsVisited>
+void DFS
+(
+  Graph const &graph,
+  IsVisited &is_visited,
+  long const source
+)
+{
+  std::deque<long> nodes;
+  nodes.emplace_back(source);
+  while (!nodes.empty())
+  {
+    auto const from {nodes.back()};
+    nodes.pop_back();
+    if (!is_visited[from])
+    {
+      is_visited[from] = true;
+      for (auto const &to : graph[from])
+      {
+        nodes.emplace_back(to);
+      }
+    }
+  }
+  return;
+}
+
+string CheckIfConnected
+(
+  long nodes_size,
+  std::vector<std::vector<long>> edges,
+  long x,
+  long y
+)
+{
+  std::vector<std::vector<long>> graph(nodes_size);
+  for (auto const &edge : edges)
+  {
+    graph[edge[0]].emplace_back(edge[1]);
+    graph[edge[1]].emplace_back(edge[0]);
+  }
+  std::vector<bool> is_visited(nodes_size, false);
+  DFS(graph, is_visited, x);
+  if (is_visited[y])
+  {
+    return "yes";
+  }
+  return "no";
+}
+
+// alternative 3 (DFS - recursive version) provided by TA 鄧晉杰
+/* note: this alternative is limited if depth of recursion is too large */
+template <typename Graph, typename IsVisited>
+void RecursiveDFS
+(
+  Graph const &graph,
+  IsVisited &is_visited,
+  long const from
+)
+{
+  is_visited[from] = true;
+  for (auto const &to : graph[from])
+  {
+    if (!is_visited[to])
+    {
+      RecursiveDFS(graph, is_visited, to);
+    }
+  }
+  return;
+}
+
+string CheckIfConnected
+(
+  long nodes_size,
+  std::vector<std::vector<long>> edges,
+  long x,
+  long y
+)
+{
+  std::vector<std::vector<long>> graph(nodes_size);
+  for (auto const &edge : edges)
+  {
+    graph[edge[0]].emplace_back(edge[1]);
+    graph[edge[1]].emplace_back(edge[0]);
+  }
+  std::vector<bool> is_visited(nodes_size, false);
+  RecursiveDFS(graph, is_visited, x);
+  if (is_visited[y])
+  {
+    return "yes";
+  }
+  return "no";
+}
+
+// alternative provided by TA 李旺陽
 string CheckIfConnected(long nodes_size, vector<vector<long>> edges, long x, long y) {
 
     vector<vector<int>> E(nodes_size);
@@ -118,7 +153,7 @@ string CheckIfConnected(long nodes_size, vector<vector<long>> edges, long x, lon
         E[e].emplace_back(s);
     }
 
-    vector<bool> used(node_size, false);
+    vector<bool> used(nodes_size, false);
     stack<int> st;
 
     st.emplace(x);
